75da56929985cb628da91a2cd7b8b24c6c436ee2
ony <ogbuebele.ony@hotmail.com>
2023-05-04 01:11:02 +0200

    Add comments

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import randomColor from 'randomcolor';
 import { useState } from 'react';
 
 export default function App() {
+  // Is the default state variable and setter randomColor
+  // Creates random Color when you reload the page
   const [color, setColor] = useState(randomColor());
+  // Function that sets the color value to randomColor() and creates a variable that has the same value
   const handleClick = () => {
     const generatedColor = randomColor();
     setColor(generatedColor);
   };
 
+  // Gradient for part of my Title
   const gradient = 'linear-gradient(45deg, #5f00b8, #ff2d55, #ffa300)';
-
+  // Css Properties in an Object that is used to style the h1 span Title
   const userGradient = {
     background: gradient,
     WebkitBackgroundClip: 'text',
